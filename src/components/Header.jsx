d14052685d08be42a5d2870223c2ecf5b7413393
precious1-2 <adekoyaprecious180@gmail.com>
2024-09-16 19:23:35 +0100

    revised it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,20 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
-import link from "../assets/link.svg";
-import tweet from "../assets/twitter.svg";
 import { Nav } from "react-bootstrap";
 import "../styles/header.css";
+const DOWNLOAD_RESUME = "/resume.pdf"
 
 const Header = () => {
+  const downloadFileAtURL = (url) => {
+    const fileName = url.split('/').pop()
+    const aTag = document.createElement('a')
+    aTag.href = url
+    aTag.setAttribute('download', fileName)
+    document.body.appendChild(aTag)
+    aTag.click()
+    aTag.remove()
+  }
   return (
     <div className="wis">
       <Navbar expand="lg" className="bg-body-tertiary">
@@ -32,6 +40,9 @@ const Header = () => {
               <a href="#project" className="lin">
                 Projects
               </a>
+              <Link className="download" >
+            <button  onClick={()=>{downloadFileAtURL(DOWNLOAD_RESUME)}}>DOWNLOAD RESUME</button>
+          </Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
